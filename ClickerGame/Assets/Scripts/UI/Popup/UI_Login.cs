fcaaa4f82263e4fedd1cef5a29fde8f9da485961
mogun1568 <kmg013329@naver.com>
2025-08-17 17:03:01 +0900

    계정 삭제 요청 기능 추가
    계정 삭제 버튼을 누르면 구글 폼으로 연결되게 함
    
    로그인 시 보이지 않는 에러가 있음. 확인 필요
    Error Unity FormatException: The input is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters.

--- ClickerGame/Assets/Scripts/UI/Popup/UI_Login.cs
+++ ClickerGame/Assets/Scripts/UI/Popup/UI_Login.cs
@@ -1,5 +1,5 @@
-using Cysharp.Threading.Tasks;
 using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -8,7 +8,8 @@ public class UI_Login : UI_Popup
     enum Buttons
     {
         Button_Log,
-        Button_Close
+        Button_Close,
+        Text_DeleteAccount
     }
 
     enum Texts
@@ -18,6 +19,7 @@ public class UI_Login : UI_Popup
     }
 
     private string _loginText, _infoText;
+    private string _deleteAccountUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdefxubR3y8fGUWwL-D8wumlA-7M0dHRIk3CttV8n2W6fM-dg/viewform?usp=header";
 
     void Awake()
     {
@@ -33,13 +35,36 @@ public class UI_Login : UI_Popup
 
         // Bind를 Button을로 했기 때문에 GetObject로 안됨
         BindEvent(GetButton((int)Buttons.Button_Log).gameObject, (PointerEventData data) => { Log(); }, Define.UIEvent.Click);
+        BindEvent(GetButton((int)Buttons.Text_DeleteAccount).gameObject, (PointerEventData data) => { DeleteAccount(); }, Define.UIEvent.Click);
         BindEvent(GetButton((int)Buttons.Button_Close).gameObject, (PointerEventData data) => { ClosePopupUI(); }, Define.UIEvent.Click);
+
+        GetButton((int)Buttons.Text_DeleteAccount).gameObject.SetActive(false);
 
         // 아마 로그인 관련 함수들이 비동기라 늦게 갱신될거 같은데 어케 할지 고민 중
         //if (Managers.Firebase.auth.CurrentUser == null)
         //    GetText((int)Texts.Text_Info).text = "로그인되어 있지 않습니다.";
         //else
         //    GetText((int)Texts.Text_Info).text = "환영합니다!";
+
+        TextInit();
+    }
+
+    public void TextInit()
+    {
+        if (Managers.Firebase.GoogleLogIn)
+        {
+            _loginText = "GOOGLE 로그아웃";
+            _infoText = "환영합니다";
+            GetButton((int)Buttons.Text_DeleteAccount).gameObject.SetActive(true);
+        }
+        else
+        {
+            _loginText = "GOOGLE 로그인";
+            _infoText = "로그인되어 있지 않습니다.";
+        }
+
+        GetText((int)Texts.Text_Login).text = _loginText;
+        GetText((int)Texts.Text_Info).text = _infoText;
     }
 
     private void Log()
@@ -56,25 +81,8 @@ public class UI_Login : UI_Popup
         Managers.UI.ShowPopupUI<UI_Notice>("Popup_Notice").UIInit(str);
     }
 
-    public async UniTask TextInitAsync()
+    private void DeleteAccount()
     {
-        //if (_infoText != "")
-        //    return;
-
-        await UniTask.WaitUntil(() => Managers.Firebase.CheckFirebaseDone);
-
-        if (Managers.Firebase.auth.CurrentUser == null)
-        {
-            _loginText = "GOOGLE 로그인";
-            _infoText = "로그인되어 있지 않습니다.";
-        }
-        else
-        {
-            _loginText = "GOOGLE 로그아웃";
-            _infoText = "환영합니다";
-        }
-
-        GetText((int)Texts.Text_Login).text = _loginText;
-        GetText((int)Texts.Text_Info).text = _infoText;
+        Application.OpenURL(_deleteAccountUrl);
     }
 }
